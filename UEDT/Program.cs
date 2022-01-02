f4307ff36db5afa9ea91c8a2f5e51b49f2a5b5b8
JFK <filip.kubajewski@infotech.edu.pl>
2022-01-02 17:39:41 +0100

    Wprowadzam coś żeby rozpocząć wreszcie ten fantstyczny projekt

--- UEDT/Program.cs
+++ UEDT/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+//Pobieranie mapy do gry
+class Map
+{
+    int x;
+    int y;
+    //mapfile to ścieżka z której będzie pobierana mapa do gry
+    string mapfile;
+    static string[] downloadmap = File.ReadAllLines(@"");
+    string[,] map = new string[downloadmap.Length, downloadmap[0].Length];
+    public void printMap()
+    {
+        //kod do ściagania mapy
+    }
+
+    public void move()
+    {
+        //kod do chodzenia po mapie
+    }
+}
